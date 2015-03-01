b2cacef9a4fc8d6e960cba7734a1738429c9490c
pola3s <paulina.serwinska@interia.pl>
2015-03-01 16:52:29 +0100

    Stworzenie bazydanych

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,26 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+City.create!(name: 'Gdynia',
+             short_name: 'Gdy')
+
+City.create!(name: 'Sopot',
+             short_name: 'Spt')
+
+City.create!(name: 'Poznań',
+             short_name: 'Pzn')
+
+City.create!(name: 'Wrocław',
+             short_name: 'Wro')
+
+City.create!(name: 'Zakopane',
+             short_name: 'Zkp')
+
+City.create!(name: 'Rzeszów',
+             short_name: 'Rze')
+
+City.create!(name: 'Łódź',
+             short_name: 'Łdź')
+
+
+
